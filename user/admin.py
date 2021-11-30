5a67a34aad6463fc0b67513422ff78e8cece39ed
svmr <saidamir2020t@gmail.com>
2021-12-01 00:59:13 +0500

    rating and comment added

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from user.models import User
+
+
+class UserAdminModel(admin.ModelAdmin):
+    list_display = ( 'id', 'full_name', 'phone', 'role', )
+
+
+admin.site.register(User, UserAdminModel)
