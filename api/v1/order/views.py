467208f416d3c4cd00df818f788732aee1b22685
svmr <saidamir2020t@gmail.com>
2021-12-01 21:40:25 +0500

    order api fixed

--- api/v1/order/views.py
+++ api/v1/order/views.py
@@ -24,7 +24,9 @@ class ProductOrderListApiView(ListAPIView):
     """ List of products in basket """
 
     serializer_class = ProductOrderListSerializer
-    queryset = ProductOrder.objects.all()
+
+    def get_queryset(self):
+        return ProductOrder.objects.filter(user=self.request.user)
 
 
 class ProductOrderCreateApiView(CreateAPIView):
@@ -38,27 +40,34 @@ class ProductOrderUpdateApiView(CreateAPIView):
     """ Update product to add to basket """
 
     serializer_class = ProductOrderCreateSerializer
-    queryset = ProductOrder.objects.all()
+
+    def get_queryset(self):
+        return ProductOrder.objects.filter(user=self.request.user)
 
 
 class ProductOrderDetailApiView(RetrieveAPIView):
     """ Detail of product in basket """
 
     serializer_class = ProductOrderDetailSerializer
-    queryset = ProductOrder.objects.all()
+
+    def get_queryset(self):
+        return ProductOrder.objects.filter(user=self.request.user)
 
 
 class ProductOrderDestroyApiView(CreateAPIView):
     """ Delete product from basket """
 
-    queryset = ProductOrder.objects.all()
+    def get_queryset(self):
+        return ProductOrder.objects.filter(user=self.request.user)
 
 
 class BasketListApiView(ListAPIView):
     """ List of baskets """
 
     serializer_class = BasketListSerializer
-    queryset = Basket.objects.all()
+
+    def get_queryset(self):
+        return Basket.objects.filter(user=self.request.user)
 
 
 class BasketCreateApiView(CreateAPIView):
@@ -72,20 +81,25 @@ class BasketUpdateApiView(UpdateAPIView):
     """ Update basket """
 
     serializer_class = BasketUpdateSerializer
-    queryset = Basket.objects.all()
+
+    def get_queryset(self):
+        return Basket.objects.filter(user=self.request.user)
 
 
 class BasketDetailApiView(RetrieveAPIView):
     """ Detail of baskets """
 
     serializer_class = BasketDetailSerializer
-    queryset = Basket.objects.all()
+
+    def get_queryset(self):
+        return Basket.objects.filter(user=self.request.user)
 
 
 class BasketDestroyApiView(DestroyAPIView):
     """ Delete basket """
 
-    queryset = Basket.objects.all()
+    def get_queryset(self):
+        return Basket.objects.filter(user=self.request.user)
 
 
 class OrderCreateApiView(CreateAPIView):
@@ -99,24 +113,31 @@ class OrderListApiView(ListAPIView):
     """ List of orders """
 
     serializer_class = OrderListSerializer
-    queryset = Order.objects.all()
+
+    def get_queryset(self):
+        return Order.objects.filter(user=self.request.user)
 
 
 class OrderUpdateApiView(UpdateAPIView):
     """ Update order """
 
     serializer_class = OrderUpdateSerializer
-    queryset = Order.objects.all()
+
+    def get_queryset(self):
+        return Order.objects.filter(user=self.request.user)
 
 
 class OrderDetailApiView(RetrieveAPIView):
     """ Detail of order """
 
     serializer_class = OrderDetailSerializer
-    queryset = Order.objects.all()
+
+    def get_queryset(self):
+        return Order.objects.filter(user=self.request.user)
 
 
 class OrderDestroyApiView(DestroyAPIView):
     """ Destroy order """
 
-    queryset = Order.objects.all()
+    def get_queryset(self):
+        return Order.objects.filter(user=self.request.user)
