5d2035b7f02c57b30ef91485c8d06d8e3cc6de69
svmr <saidamir2020t@gmail.com>
2021-12-01 21:57:43 +0500

    filterset class added

--- sprinter_settings/settings.py
+++ sprinter_settings/settings.py
@@ -46,6 +46,7 @@ THIRD_PARTY_APPS = [
     'rest_framework_simplejwt',
     'drf_yasg',
     'colorfield',
+    'django_filters'
 ]
 
 LOCALE_APPS = [
@@ -171,4 +172,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated'
     ],
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ),
 }
