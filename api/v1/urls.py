3731d8be7afc0769cb8e998fcdf75e782b226f75
ozod_saidov <ozodsaidov2@gmail.com>
2021-12-02 16:43:23 +0500

    product api views added

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -3,4 +3,5 @@ from django.urls import path, include
 urlpatterns = [
     path('user/', include('api.v1.user.urls')),
     path('order/', include('api.v1.order.urls')),
+    path('product/', include('api.v1.product.urls')),
 ]
