71d3cd8a7898ad778dee426757f7d210d62c2649
svmr <saidamir2020t@gmail.com>
2021-12-11 00:21:10 +0500

    .env

--- sprinter_settings/settings.py
+++ sprinter_settings/settings.py
@@ -236,7 +236,7 @@ EMAIL_USE_SSL = True
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": os.getenv('redis_location'),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
