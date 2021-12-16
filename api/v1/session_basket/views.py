dc26214cdf8e5d6a732202c2700db98b6585996a
svmr <saidamir2020t@gmail.com>
2021-12-17 03:13:05 +0500

    session list method done

--- api/v1/session_basket/views.py
+++ api/v1/session_basket/views.py
@@ -5,14 +5,17 @@ from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from django.http import JsonResponse
-from core.models import Product
+from core.models import Product, ProductColor, ProductParam
+from sprinter_settings import settings
 from .basket import Basket
 from django.http import HttpResponse
 from rest_framework.views import APIView
 from django.core.exceptions import ValidationError
 
 from .validation import SessionProductOrderValidation
-
+from ..product.serializers import ProductRetrieveSerializer, ProductShortDetailSerializer, ColorSerializer, \
+    ProductParamSerializer
+from django.db.models import Sum
 
 class TemporaryBasketCreateApi(APIView):
     """ Temporarily create basket in session """
@@ -56,7 +59,7 @@ class TemporaryBasketUpdateApi(APIView):
                 basket.update(product=product.last(), params=params, color_id=color_id, quantity=quantity, id=id)
             return JsonResponse(basket.current_product, safe=False)
         else:
-            return Response('Product not found')
+            return JsonResponse('Product not found', safe=False)
 
 
 class TemporaryBasketListApi(APIView):
@@ -65,8 +68,31 @@ class TemporaryBasketListApi(APIView):
     permission_classes = [AllowAny, ]
 
     def get(self, request):
-        basket = Basket(request=request)
-        return JsonResponse(basket.basket, safe=False)
+        basket = Basket(request=request).basket
+        data = []
+        for b in basket:
+            product = Product.objects.filter(id=b.get('product_id', 0))
+            color = ProductColor.objects.filter(id=b.get('color_id', 0))
+            params = ProductParam.objects.filter(id__in=b.get('params', []))
+            if product.exists() and color.exists():
+                product = product.last()
+                temp = {}
+                url_scheme = '{}://{}{}{}'.format(request.scheme, request.get_host(),
+                                                  settings.MEDIA_URL, color.last().images.last().image)
+                temp['id'] = b.get('id')
+                temp['product'] = ProductShortDetailSerializer(instance=product, many=False).data
+                temp['color'] = dict(color=color.last().color, image=url_scheme)
+                temp['quantity'] = b.get('quantity')
+                temp['params'] = ProductParamSerializer(instance=params, many=True).data
+
+                additional_price = product.prices.filter(
+                    param__in=b.get('params')).aggregate(additional_price=Sum('price'))
+                product_price = product.price
+                if additional_price.get('additional_price'):
+                    product_price += additional_price['additional_price']
+                temp['product_price'] = product_price
+                data.append(temp)
+        return JsonResponse(data, safe=False)
 
 
 class TemporaryBasketDeleteApi(APIView):
