60cfd8a360da814ee4db5d9e4d4e0a541b5d4591
svmr <saidamir2020t@gmail.com>
2021-12-19 16:40:30 +0500

    some fixes in model

--- core/models/product.py
+++ core/models/product.py
@@ -180,3 +180,6 @@ class Delivery(Base):
 class Region(Base):
     name = models.CharField(max_length=255, unique=True)
     is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.name}'
