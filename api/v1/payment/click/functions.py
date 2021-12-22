3275d98f584daeca04d271fbc3ce650495283585
svmr <saidamir2020t@gmail.com>
2021-12-22 22:35:09 +0500

    click added

--- api/v1/payment/click/functions.py
+++ api/v1/payment/click/functions.py
@@ -1,9 +1,11 @@
 import os
 
 from clickuz import ClickUz
+from common.static_data import PaymentType
 
 
 def click_url(order):
     return_url = os.getenv('WEB_URL')
-    click = ClickUz.generate_url(order_id=order.id, amount=order.price, return_url=return_url)
-    return click
+    click_url = ClickUz.generate_url(order_id=order.id, amount=order.price, return_url=return_url)
+    url = dict(url=click_url, type=PaymentType.CLICK)
+    return url
