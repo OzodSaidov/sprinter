5bae0b8aaef051292d4c7b418ef2781d1324a443
ozod_saidov <ozodsaidov2@gmail.com>
2022-01-02 01:00:53 +0500

    slider list fixed

--- api/v1/services/get_endpoint.py
+++ api/v1/services/get_endpoint.py
@@ -3,5 +3,5 @@ from rest_framework.request import Request
 from sprinter_settings import settings
 
 
-def get_image_endpoint(request: Request):
+def get_image_endpoint(request: Request) -> str:
     return "%s://%s%s" % (request.scheme, request.get_host(), settings.MEDIA_URL)
