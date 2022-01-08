073a4576611ef392283974c5b0c1d89e9d899f9d
svmr <saidamir2020t@gmail.com>
2022-01-08 16:53:16 +0500

    click fixed

--- core/views.py
+++ core/views.py
@@ -63,6 +63,8 @@ class ClickCheckOrder(ClickUz):
             order = Order.objects.filter(id=order_id, payment_status=PaymentStatus.WAITING).first()
             if not order:
                 return self.ORDER_NOT_FOUND
+            print(order.payment_type)
+            print(order.payment_type == PaymentType.CLICK)
             if not order.payment_type != PaymentType.CLICK:
                 logger.warning(f'Order payment type is not click: {order_id}')
                 return self.ORDER_NOT_FOUND
