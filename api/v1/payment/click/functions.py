5d966d45326cad8dd46e9b8bc067b57b0c20dc40
svmr <saidamir2020t@gmail.com>
2022-01-13 21:00:10 +0500

    click and payme return url

--- api/v1/payment/click/functions.py
+++ api/v1/payment/click/functions.py
@@ -5,7 +5,6 @@ from common.static_data import PaymentType
 
 
 def click_url(order):
-    return_url = os.getenv('WEB_URL')
-    click_url = ClickUz.generate_url(order_id=order.id, amount=order.price, return_url=return_url)
+    click_url = ClickUz.generate_url(order_id=order.id, amount=order.price, return_url='https://sprinter.uz/success-payment')
     url = dict(url=click_url, type=PaymentType.CLICK)
     return url
